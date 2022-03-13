2199980a28b389228a57e770341fc0d9be7782d6
太田瑛棋 <nezirekokkai0929@icloud.com>
2022-03-13 15:17:47 +0900

    単語取得js制作
    ほげほげ

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let hoge;
+
 async function callApi() {
     const res = await fetch("https://random-words-api.vercel.app/word");
     const users = await res.json();
     //console.log(users)
-    hoge = users[0].word;
-    console.log(hoge);
-    
+    let newWord;
+    newWord = users[0].word;//newWordに問題単語取得
+    console.log(newWord);
+
+    //target = document.getElementById("questionWord");
+    //target.innerText = newWord;
   };
   callApi();
+
